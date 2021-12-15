a49fa8d88ccbf1a9b1ed28e278ac2edb86c09c14
tar1971 <96209005+tar1971@users.noreply.github.com>
2021-12-15 23:41:09 +0200

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+#wget -q "--no-check-certificate" https://raw.githubusercontent.com/emil237/ajpanel/main/installer.sh -O - | /bin/sh
+
+##########################################
+version=3.2.3
+#############################################################
+TEMPATH=/tmp
+OPKGINSTALL="opkg install --force-reinstall"
+MY_IPK="enigma2-plugin-extensions-ajpanel_v3.2.3_all.ipk"
+MY_DEB="enigma2-plugin-extensions-ajpanel_v3.2.3_all.deb"
+MY_URL="https://raw.githubusercontent.com/emil237/ajpanel/main"
+# remove old version #
+rm -rf /usr/lib/enigma2/python/Plugins/Extensions/AJPan
+
+echo ""
+# Download and install plugin
+cd /tmp
+set -e
+     wget "$MY_URL/$MY_IPK"
+  wait
+     wget "$MY_URL/$MY_DEB"
+
+ if which dpkg > /dev/null 2>&1; then
+		dpkg -i --force-overwrite $MY_DEB; apt-get install -f -y
+	else
+		$OPKGINSTALL $MY_IPK
+	fi
+echo "================================="
+set +e
+cd ..
+wait
+rm -f /tmp/*.ipk
+rm -f /tmp/*.deb
+	if [ $? -eq 0 ]; then
+echo ">>>>  SUCCESSFULLY INSTALLED <<<<"
+fi
+		echo "********************************************************************************"
+echo "   UPLOADED BY  >>>>   EMIL_NABIL "   
+sleep 4;
+		echo ". >>>>         RESTARING     <<<<"
+echo "**********************************************************************************"
+wait
+killall -9 enigma2
+exit 0
+
+
+
