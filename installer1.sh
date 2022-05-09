c40bc1fe15c711285f5b81f77923abc7f789fa70
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-05-10 01:46:52 +0200

    Update installer1.sh

--- installer1.sh
+++ installer1.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 ######################################################################################
-## Command=wget https://raw.githubusercontent.com/tarekzoka/ajpanel/main/installer.sh -O - | /bin/sh
+## Command=wget https://raw.githubusercontent.com/tar1971/ajpanel/main/installer1.sh -O - | /bin/sh
 ##
 ###########################################
 ###########################################
 #!/bin/sh
 echo
 
-opkg install --force-overwrite  https://github.com/tarekzoka/ajpanel/blob/main/enigma2-plugin-extensions-ajpanel_v5.0.1_all.ipk?raw=true
+opkg install --force-overwrite  https://github.com/tar1971/ajpanel/blob/main/enigma2-plugin-extensions-ajpanel_v5.0.1_all.ipk?raw=true
 wait
 #!/bin/sh
 #
 
-wget -O /tmp/ajpanel_v5.1.0_all.deb "https://github.com/tarekzoka/ajpanel/blob/main/enigma2-plugin-extensions-ajpanel_v5.1.0_all.deb?raw=true"
+wget -O /tmp/ajpanel_v5.1.0_all.deb "https://github.com/tar1971/ajpanel/blob/main/enigma2-plugin-extensions-ajpanel_v5.1.0_all.deb?raw=true"
 wait
 apt-get update ; dpkg -i /tmp/*.deb ; apt-get -y -f install
 wait
